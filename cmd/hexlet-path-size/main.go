e3d39d7e310c71073afb2af8dc113bfdccc07eb7
Konstantin Kukharuk <Konstantin@Kukharuk.ru>
2026-04-02 14:11:26 +0300

    Add function GetSize()

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
+
+	pathsize "code"
 
 	"github.com/urfave/cli/v3"
 )
 
 func main() {
 	cmd := &cli.Command{
-		Name:  "hexlet-path-size",
-		Usage: "print size of a file or directory",
-		Flags: []cli.Flag{},
+		Name:      "hexlet-path-size",
+		Usage:     "print size of a file or directory",
+		ArgsUsage: "<path>",
 		Action: func(ctx context.Context, c *cli.Command) error {
+			p := c.Args().First()
+			if p == "" {
+				return fmt.Errorf("path is required")
+			}
+			size, err := pathsize.GetSize(p)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("%s\t%s\n", pathsize.FormatSize(size), p)
 			return nil
 		},
 	}
--- path_size.go
+++ path_size.go
@@ -1,1 +1,54 @@
 package pathsize
+
+import (
+	"fmt"
+	"os"
+)
+
+func GetSize(path string) (int64, error) {
+	info, err := os.Stat(path)
+	if err != nil {
+		return 0, err
+	}
+
+	if !info.IsDir() {
+		return info.Size(), nil
+	}
+
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return 0, err
+	}
+
+	var total int64
+	for _, entry := range entries {
+		info, err := entry.Info()
+		if err != nil {
+			return 0, err
+		}
+		if !info.IsDir() {
+			total += info.Size()
+		}
+	}
+
+	return total, nil
+}
+
+func FormatSize(size int64) string {
+	const (
+		KB = 1024
+		MB = KB * 1024
+		GB = MB * 1024
+	)
+
+	switch {
+	case size >= GB:
+		return fmt.Sprintf("%.2f GB", float64(size)/GB)
+	case size >= MB:
+		return fmt.Sprintf("%.2f MB", float64(size)/MB)
+	case size >= KB:
+		return fmt.Sprintf("%.2f kB", float64(size)/KB)
+	default:
+		return fmt.Sprintf("%d B", size)
+	}
+}
